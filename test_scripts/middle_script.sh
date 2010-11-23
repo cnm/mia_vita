1048eae31348cc70962b0ebe98bed134559b4473
Joao Trindade <trindade.joao@gmail.com>
2010-11-23 18:33:09 +0000

    Added support for deleting routes

--- test_scripts/middle_script.sh
+++ test_scripts/middle_script.sh
@@ -1,6 +1,13 @@
 #### BATMAN ROUTING ###################
 echo RUNNING WITH BATMAN
 
+ifconfig rausbwifi up
+iwconfig rausbwifi mode managed
+sleep 3
+ifconfig rausbwifi down
+ifconfig rausbwifi up
+iwconfig rausbwifi mode ad-hoc essid teste channel 1 ap 02:0C:F1:B5:CC:5D
+
 ifconfig rausbwifi 192.168.0.2
 ifconfig rausbwifi:client down
 insmod batman-adv.ko
@@ -27,5 +34,5 @@ echo "Now my ip is 192.168.5.2 and 192.168.6.2"
 ifconfig rausbwifi:client 192.168.5.2 up
 ifconfig rausbwifi 192.168.6.2
 echo 1 > /proc/sys/net/ipv4/ip_forward
-route add -net 192.168.5.0 netmask 255.255.255.0 dev rausbwifi:client
-route add -net 192.168.6.0 netmask 255.255.255.0 dev rausbwifi
+#route add -net 192.168.5.0 netmask 255.255.255.0 dev rausbwifi:client
+#route add -net 192.168.6.0 netmask 255.255.255.0 dev rausbwifi
