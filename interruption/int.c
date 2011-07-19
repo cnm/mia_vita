998f21069b29d147545f14b803b2174d33d78c91
Joao Trindade <trindade.joao@gmail.com>
2011-07-19 20:22:06 +0100

    Some fixes

--- interruption/int.c
+++ interruption/int.c
@@ -22,62 +22,66 @@
 #include <linux/delay.h>        /* udelay */
 #include <linux/ioport.h>
 #include <linux/interrupt.h>
+#include <asm/io.h>             /* ioremap */
 
 MODULE_AUTHOR ("Joao Trindade");
 MODULE_LICENSE("GPL");
 
-#define IRQ_NUMBER 7
+#define IRQ_NUMBER 2
 #define MEMORY_REGION 0x7C000000
-#define MEMORY_SIZE 1
+#define MEMORY_SIZE 4
 
+#define MISC_REGISTER 0x76000000
+#define GPIOA_EN_ADDRESS ((MISC_REGISTER) + 0x20)  /* GPIO_A_PIN_ENABLE_REGISTER See page 187 */
+#define TEST_ADDR ((MISC_REGISTER) + 0x18)
+
+/* For testing purposes  */
+/*#undef GPIOA_EN_ADDRESS*/
+/*#define GPIOA_EN_ADDRESS TEST_ADDR */
+
+/* Function Headers*/
+void release_mem(unsigned int mem_addr, unsigned int memory_size);
 void request_memory_regions(void);
-irqreturn_t short_interrupt(int irq, void *dev_id);
+irqreturn_t interrupt(int irq, void *dev_id);
 void register_handle_interruption(void);
 void unregister_handle_interruption(void);
 void unregister_memory_region(void);
+int request_mem(unsigned int mem_addr, unsigned int size);
+int request_port(unsigned int port_addr, unsigned int size);
 
-irqreturn_t short_interrupt(int irq, void *dev_id)
+/*
+ * To handle the interruption
+*/
+irqreturn_t interrupt(int irq, void *dev_id)
 {
   printk(KERN_INFO "Inside the interruption\n");
   printk(KERN_EMERG "Inside the interruption\n");
   return IRQ_HANDLED;
 }
 
-void request_memory_regions(){
-    /* if using ports */
-    /*    if (! request_region(MEMORY_REGION, MEMORY_SIZE, "MV_INT")) {
-          printk(KERN_INFO "short: can't get I/O port address 0x%lx\n",
-          short_base);
-          return -ENODEV;
-          }*/
+/* Requests all memory necessary for the module */
+void request_memory_regions(void){
+    unsigned int i;
+    int new_addr;
 
-    /* If using memory */
-    /* if (! request_mem_region(MEMORY_REGION, MEMORY_SIZE, "MV_INT")) {
-       printk(KERN_INFO "short: can't get I/O mem address 0x%lx\n",
-       short_base);
-       return -ENODEV;
-       }
-       short_base = (unsigned long) ioremap(short_base, SHORT_NR_PORTS);
-     */
+    new_addr = request_mem(GPIOA_EN_ADDRESS, MEMORY_SIZE);
+    i = *(unsigned int *)(new_addr);
 
+    printk(KERN_INFO "Testing with address: %p --------> %x\n", (void *) new_addr, i);
     return;
 }
 
-void register_handle_interruption(){
-    int result;
-    printk(KERN_INFO "Preparing interruption number 2 %i\n", IRQ_NUMBER);
-
-    result = request_irq(IRQ_NUMBER, short_interrupt, IRQF_DISABLED, "MV_INT", NULL);
-    if (result) {
-        printk(KERN_INFO "short: can't get assigned irq %i\n",
-               IRQ_NUMBER);
-    }
+/* Unregister memory regions */
+void unregister_memory_region()
+{
+    release_mem(GPIOA_EN_ADDRESS, MEMORY_SIZE);
 }
 
-/* Finally, init and cleanup */
-int short_init(void)
+
+/* Runs when the module is initiated */
+int init(void)
 {
-  printk(KERN_INFO "starting int.\n");
+  printk(KERN_INFO "starting interruption module.\n");
 
   request_memory_regions();
 
@@ -89,28 +93,78 @@ int short_init(void)
   return 0;
 }
 
-void unregister_handle_interruption(){
-    free_irq(IRQ_NUMBER, NULL);
-}
-
-void unregister_memory_region(){
-    /*  If I use memory */
-    /*
-       iounmap((void __iomem *)MEMORY_REGION);
-       release_mem_region(MEMORY_REGION, MEMORY_SIZE);
-     */
-
-    /*release_region(MEMORY_REGION, MEMORY_SIZE);*/
-}
-
-void short_cleanup(void)
+/*
+ * Runs when the module is unloaded
+ * */
+void cleanup(void)
 {
   unregister_handle_interruption();
   /* unregister_device(); ???? */
   unregister_memory_region();
 
-  printk(KERN_INFO "leaning int.\n");
+  printk(KERN_INFO "Unregister module interruption.\n");
 }
 
-module_init(short_init);
-module_exit(short_cleanup);
+
+/*
+ * Register the interruption handler (and the IRQ number)
+ * */
+void register_handle_interruption(){
+    int result;
+
+    printk(KERN_INFO "Registering to handle IRQ number %i\n", IRQ_NUMBER);
+    result = request_irq(IRQ_NUMBER, interrupt, IRQF_DISABLED, "MV_INT", NULL);
+    if (result) {
+        printk(KERN_INFO "can't get assigned irq %i\n", IRQ_NUMBER);
+        printk(KERN_INFO "Error number result %i\n", result);
+    }
+}
+
+
+/**************************** Auxiliary Functions *****************************/
+int request_mem(unsigned int mem_addr, unsigned int size){
+    unsigned int new_mem;
+    if (! request_mem_region(mem_addr, size, "MV_INT")) {
+        printk(KERN_INFO "can't get I/O mem address 0x%x\n", mem_addr);
+        return -ENODEV;
+    }
+    new_mem = (unsigned int) ioremap(mem_addr, size);
+    printk(KERN_INFO "New memory pointer: %x\n", new_mem);
+
+    return new_mem;
+}
+
+
+int request_port(unsigned int port_addr, unsigned int size){
+    unsigned int new_mem = 0;
+
+    if (!request_region(port_addr, size, "MV_INT")) {
+        printk(KERN_INFO "can't get I/O port address 0x%x\n", port_addr);
+        return -ENODEV;
+    }
+
+    return new_mem;
+}
+
+void unregister_handle_interruption()
+{
+    free_irq(IRQ_NUMBER, NULL);
+}
+
+void release_port(unsigned int port_addr, unsigned int memory_size)
+{
+    release_region(port_addr, memory_size);
+
+}
+
+void release_mem(unsigned int mem_addr, unsigned int memory_size)
+{
+       iounmap((void __iomem *)mem_addr);
+       release_mem_region(mem_addr, memory_size);
+}
+
+/******************************** End of auxiliary functions **************************/
+
+
+module_init(init);
+module_exit(cleanup);
