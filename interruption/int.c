7e56a5cdfd0c913008c1798d309d3e3b6408e598
Joao Trindade <trindade.joao@gmail.com>
2011-07-20 11:41:56 +0100

    Setting GPIOA_EN but in negative

--- interruption/int.c
+++ interruption/int.c
@@ -27,31 +27,41 @@
 MODULE_AUTHOR ("Joao Trindade");
 MODULE_LICENSE("GPL");
 
-#define IRQ_NUMBER 2
+#define IRQ_NUMBER 13
 #define MEMORY_REGION 0x7C000000
 #define MEMORY_SIZE 4
 
 #define MISC_REGISTER 0x76000000
-#define GPIOA_EN_ADDRESS ((MISC_REGISTER) + 0x20)  /* GPIO_A_PIN_ENABLE_REGISTER See page 187 */
-#define TEST_ADDR ((MISC_REGISTER) + 0x18)
+#define GPIOA_EN_ADDRESS    ((MISC_REGISTER) + 0x20)  /* GPIO_A_PIN_ENABLE_REGISTER See page 187 */
+#define TEST_ADDR           ((MISC_REGISTER) + 0x18)
+
+#define SCL_BIT_NUMBER 13
+#define SDA_BIT_NUMBER 14
+#define SCL_MASK (1<<SCL_BIT_NUMBER)
+#define SDA_MASK (1<<SCL_BIT_NUMBER)
+
+#define GPIOA_EN_MASK (SCL_MASK | SDA_MASK)
 
 /* For testing purposes  */
 /*#undef GPIOA_EN_ADDRESS*/
 /*#define GPIOA_EN_ADDRESS TEST_ADDR */
 
 /* Function Headers*/
-void release_mem(unsigned int mem_addr, unsigned int memory_size);
+void release_mem(volatile unsigned int mem_addr, unsigned int memory_size);
 void request_memory_regions(void);
 irqreturn_t interrupt(int irq, void *dev_id);
 void register_handle_interruption(void);
 void unregister_handle_interruption(void);
 void unregister_memory_region(void);
-int request_mem(unsigned int mem_addr, unsigned int size);
+int request_mem(volatile unsigned int mem_addr, unsigned int size);
 int request_port(unsigned int port_addr, unsigned int size);
+void enable_pin_interruptions(void);
+
+unsigned int gpioa_en_new_address = 0;
 
 /*
  * To handle the interruption
-*/
+ */
 irqreturn_t interrupt(int irq, void *dev_id)
 {
   printk(KERN_INFO "Inside the interruption\n");
@@ -62,19 +72,33 @@ irqreturn_t interrupt(int irq, void *dev_id)
 /* Requests all memory necessary for the module */
 void request_memory_regions(void){
     unsigned int i;
-    int new_addr;
 
-    new_addr = request_mem(GPIOA_EN_ADDRESS, MEMORY_SIZE);
-    i = *(unsigned int *)(new_addr);
+    gpioa_en_new_address = request_mem(GPIOA_EN_ADDRESS, MEMORY_SIZE);
 
-    printk(KERN_INFO "Testing with address: %p --------> %x\n", (void *) new_addr, i);
+    i = *(unsigned int *)(gpioa_en_new_address);
+    printk(KERN_INFO "Testing with address: %p --------> %x\n", (void *) gpioa_en_new_address, i);
     return;
 }
 
 /* Unregister memory regions */
 void unregister_memory_region()
 {
-    release_mem(GPIOA_EN_ADDRESS, MEMORY_SIZE);
+  release_mem(GPIOA_EN_ADDRESS, MEMORY_SIZE);
+}
+
+/*  Set's all pins needed for interruptions */
+void enable_pin_interruptions(void)
+{
+
+
+  /* Puts GPIOA_EN bits 13 and 14 to 0 */
+  volatile unsigned int *p;
+  p = (unsigned int *) gpioa_en_new_address;
+
+  printk(KERN_INFO "\t\tBEFORE: %x \n", *p);
+  *p |= GPIOA_EN_MASK;
+  printk(KERN_INFO "\t\tAfter: %x \n", *p);
+
 }
 
 
@@ -84,11 +108,10 @@ int init(void)
   printk(KERN_INFO "starting interruption module.\n");
 
   request_memory_regions();
-
-  /* Here we register our device - should not fail thereafter */
-  //register_device()???*/
-  /* Request to handle the interruption */
+  enable_pin_interruptions();
   register_handle_interruption();
+
+
 
   return 0;
 }
@@ -122,7 +145,7 @@ void register_handle_interruption(){
 
 
 /**************************** Auxiliary Functions *****************************/
-int request_mem(unsigned int mem_addr, unsigned int size){
+int request_mem(volatile unsigned int mem_addr, unsigned int size){
     unsigned int new_mem;
     if (! request_mem_region(mem_addr, size, "MV_INT")) {
         printk(KERN_INFO "can't get I/O mem address 0x%x\n", mem_addr);
@@ -148,19 +171,19 @@ int request_port(unsigned int port_addr, unsigned int size){
 
 void unregister_handle_interruption()
 {
-    free_irq(IRQ_NUMBER, NULL);
+  free_irq(IRQ_NUMBER, NULL);
 }
 
 void release_port(unsigned int port_addr, unsigned int memory_size)
 {
-    release_region(port_addr, memory_size);
+  release_region(port_addr, memory_size);
 
 }
 
-void release_mem(unsigned int mem_addr, unsigned int memory_size)
+void release_mem(volatile unsigned int mem_addr, unsigned int memory_size)
 {
-       iounmap((void __iomem *)mem_addr);
-       release_mem_region(mem_addr, memory_size);
+  iounmap((void __iomem *)mem_addr);
+  release_mem_region(mem_addr, memory_size);
 }
 
 /******************************** End of auxiliary functions **************************/
