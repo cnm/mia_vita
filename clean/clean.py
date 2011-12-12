143e140d7c00622a1d6ade8dff81557e060c4666
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2011-12-12 23:26:18 +0000

    Minimal cleaning process

--- clean/clean.py
+++ clean/clean.py
@@ -1,7 +1,39 @@
-from fabric.api import run
+from fabric.api import *
+
+DO_AUTOREMOVE=True
 
 def strip_modules():
-    run('strip -s `find /lib/modules/2.6.24.4/ -name "*.ko"`')
+    run('u=`uname -r`; strip -s `find /lib/modules/$u/ -name "*.ko"`')
 
+def remove_stripped_modules():
+    run('u=`uname -r`;rm -r /lib/modules/$u-stip')
+
+def remove_apache():
+    run('apt-get -y purge apache2')
+    if DO_AUTOREMOVE:
+        run('apt-get -y autoremove')
+
+def remove_avahi():
+    run('apt-get -y purge avahi-utils avahi-autoipd avahi-daemon')
+    if DO_AUTOREMOVE:
+        run('apt-get -y autoremove')
+
+def remove_nfs():
+    run('apt-get -y purge nfs-common libnfsidmap2')
+    if DO_AUTOREMOVE:
+        run('apt-get -y autoremove')
+
+def do_reboot():
+    run('reboot')
+
+@parallel
 def clean_arm():
-    run('ls banana')
+    global DO_AUTOREMOVE
+    strip_modules()
+    remove_stripped_modules()
+    DO_AUTOREMOVE=False
+    remove_apache()
+    remove_avahi()
+    remove_nfs()
+    run('apt-get -y autoremove')
+    do_reboot()
