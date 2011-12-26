b691ca529f8281025539d11b7d2aead06adea70a
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2011-12-26 13:07:40 +0000

    added manual of developed software

---
+++ documentation/manuals/fred/packet.py
@@ -0,0 +1,12 @@
+{
+	"12:0" : {"timestamp" : 12345, "air_time" : 12344, 
+			  "sequence" : 0, "fails" : 0, 
+			  "retries" : 0, "sample_1" : 1234, 
+			  "sample_2" : 1234, "sample_3" : 12345, 
+			  "node_id": 12},
+	"13:5" : {"timestamp" : 12345, "air_time" : 12344, 
+			  "sequence" : 5, "fails" : 0, 
+			  "retries" : 0, "sample_1" : 1234, 
+			  "sample_2" : 1234, "sample_3" : 12345, 
+			  "node_id": 13}	
+}
