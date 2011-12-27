7f51e731876f596d1e08f0c281c9c62f94e173d2
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2011-12-27 19:40:32 +0000

    Ok o problema tem haver com implementação, mas é corrigivel em runtime. Necessito de explicar isto no manual e ainda testar.

--- fred_framework/hooks.c
+++ fred_framework/hooks.c
@@ -31,61 +31,61 @@
 /*Each matching function returns 0 if match fails and 1 otherwise.*/
 
 static uint8_t match_src_addr(filter* f, struct sk_buff* skb) {
-	filter_specs *sp = f->filter_specs;
-	struct iphdr* ip_header = ip_hdr(skb);
+  filter_specs *sp = f->filter_specs;
+  struct iphdr* ip_header = ip_hdr(skb);
 
-	if ((sp->filter_by & FILTER_BY_SRC_ADDR) && (sp->src_addr
-			!= ip_header->saddr))
-		return 0;
-	return 1;
+  if ((sp->filter_by & FILTER_BY_SRC_ADDR) && (sp->src_addr
+					       != ip_header->saddr))
+    return 0;
+  return 1;
 }
 
 static uint8_t match_dst_addr(filter* f, struct sk_buff* skb) {
-	filter_specs* sp = f->filter_specs;
-	struct iphdr* ip_header = ip_hdr(skb);
+  filter_specs* sp = f->filter_specs;
+  struct iphdr* ip_header = ip_hdr(skb);
 
-	if ((sp->filter_by & FILTER_BY_DST_ADDR) && (sp->dst_addr
-			!= ip_header->daddr))
-		return 0;
-	return 1;
+  if ((sp->filter_by & FILTER_BY_DST_ADDR) && (sp->dst_addr
+					       != ip_header->daddr))
+    return 0;
+  return 1;
 }
 
 static uint8_t match_proto_and_ports(filter* f, struct sk_buff* skb) {
-	filter_specs* sp = f->filter_specs;
-	struct iphdr* ip_header = ip_hdr(skb);
-	struct udphdr* udp_header;
+  filter_specs* sp = f->filter_specs;
+  struct iphdr* ip_header = ip_hdr(skb);
+  struct udphdr* udp_header;
 
-	if (sp->filter_by & FILTER_BY_L4_PROTO) {
-		if (ip_header->protocol != sp->protocol)
-			return 0;
-		else {
-			switch (sp->protocol) {
-			case IPPROTO_UDP:
-				udp_header = (struct udphdr*) (((char*) ip_header)
-						+ (ip_header->ihl << 2));
-				if ((sp->filter_by & FILTER_BY_DST_PORT) && (sp->dst_port
-						!= udp_header->dest))
-					return 0;
-				if ((sp->filter_by & FILTER_BY_SRC_PORT) && (sp->src_port
-						!= udp_header->source))
-					return 0;
-				break;
-			}
-		}
-	}
-	return 1;
+  if (sp->filter_by & FILTER_BY_L4_PROTO) {
+    if (ip_header->protocol != sp->protocol)
+      return 0;
+    else {
+      switch (sp->protocol) {
+      case IPPROTO_UDP:
+	udp_header = (struct udphdr*) (((char*) ip_header)
+				       + (ip_header->ihl << 2));
+	if ((sp->filter_by & FILTER_BY_DST_PORT) && (sp->dst_port
+						     != udp_header->dest))
+	  return 0;
+	if ((sp->filter_by & FILTER_BY_SRC_PORT) && (sp->src_port
+						     != udp_header->source))
+	  return 0;
+	break;
+      }
+    }
+  }
+  return 1;
 }
 
 static uint8_t match_filter(filter* f, struct sk_buff* skb) {
-	if (!match_src_addr(f, skb))
-		return 0;
+  if (!match_src_addr(f, skb))
+    return 0;
 
-	if (!match_dst_addr(f, skb))
-		return 0;
+  if (!match_dst_addr(f, skb))
+    return 0;
 
-	if (!match_proto_and_ports(f, skb))
-		return 0;
-	return 1;
+  if (!match_proto_and_ports(f, skb))
+    return 0;
+  return 1;
 }
 
 /*This is to avoid some kernel compatibility issues. Use the lower case versions.*/
@@ -126,176 +126,176 @@ static struct nf_hook_ops nf_ip_pre_routing;
 static struct nf_hook_ops nf_ip_post_routing;
 
 static unsigned int nf_ip_local_out_hook(unsigned int hooknum,
-		struct sk_buff *skb, const struct net_device *in,
-		const struct net_device *out, int(*okfn)(struct sk_buff*)) {
-	filter_chain c = get_chain(LOCAL_OUT_CHAIN);
-	klist_iterator* it;
-	filter* f;
-	unsigned int retval;
+					 struct sk_buff *skb, const struct net_device *in,
+					 const struct net_device *out, int(*okfn)(struct sk_buff*)) {
+  filter_chain c = get_chain(LOCAL_OUT_CHAIN);
+  klist_iterator* it;
+  filter* f;
+  unsigned int retval;
 
-	if (c.len == 0)
-		return NF_ACCEPT;
+  if (c.len == 0)
+    return NF_ACCEPT;
 
-	it = make_klist_iterator(c.filters);
-	while (klist_iterator_has_next(it)) {
-		f = (filter*) (klist_iterator_next(it))->data;
-		if (match_filter(f, skb)) {
-			retval = apply_filter(f, hooknum, skb, in, out, okfn);
-			if (retval != NF_ACCEPT) {
-				free_klist_iterator(it);
-				return retval;
-			}
-		}
-	}
+  it = make_klist_iterator(c.filters);
+  while (klist_iterator_has_next(it)) {
+    f = (filter*) (klist_iterator_next(it))->data;
+    if (match_filter(f, skb)) {
+      retval = apply_filter(f, hooknum, skb, in, out, okfn);
+      if (retval != NF_ACCEPT) {
 	free_klist_iterator(it);
+	return retval;
+      }
+    }
+  }
+  free_klist_iterator(it);
 
-	return NF_ACCEPT;
+  return NF_ACCEPT;
 }
 
 static unsigned int nf_ip_local_in_hook(unsigned int hooknum,
-		struct sk_buff *skb, const struct net_device *in,
-		const struct net_device *out, int(*okfn)(struct sk_buff*)) {
-	filter_chain c = get_chain(LOCAL_IN_CHAIN);
-	klist_iterator* it;
-	filter* f;
-	unsigned int retval;
+					struct sk_buff *skb, const struct net_device *in,
+					const struct net_device *out, int(*okfn)(struct sk_buff*)) {
+  filter_chain c = get_chain(LOCAL_IN_CHAIN);
+  klist_iterator* it;
+  filter* f;
+  unsigned int retval;
 
-	if (c.len == 0)
-		return NF_ACCEPT;
+  if (c.len == 0)
+    return NF_ACCEPT;
 
-	it = make_klist_iterator(c.filters);
-	while (klist_iterator_has_next(it)) {
-		f = (filter*) (klist_iterator_next(it))->data;
-		if (match_filter(f, skb)) {
-			retval = apply_filter(f, hooknum, skb, in, out, okfn);
-			if (retval != NF_ACCEPT) {
-				free_klist_iterator(it);
-				return retval;
-			}
-		}
-	}
+  it = make_klist_iterator(c.filters);
+  while (klist_iterator_has_next(it)) {
+    f = (filter*) (klist_iterator_next(it))->data;
+    if (match_filter(f, skb)) {
+      retval = apply_filter(f, hooknum, skb, in, out, okfn);
+      if (retval != NF_ACCEPT) {
 	free_klist_iterator(it);
+	return retval;
+      }
+    }
+  }
+  free_klist_iterator(it);
 
-	return NF_ACCEPT;
+  return NF_ACCEPT;
 }
 
 static unsigned int nf_ip_forward_hook(unsigned int hooknum,
-		struct sk_buff *skb, const struct net_device *in,
-		const struct net_device *out, int(*okfn)(struct sk_buff*)) {
-	filter_chain c = get_chain(FORWARD_CHAIN);
-	klist_iterator* it;
-	filter* f;
-	unsigned int retval;
+				       struct sk_buff *skb, const struct net_device *in,
+				       const struct net_device *out, int(*okfn)(struct sk_buff*)) {
+  filter_chain c = get_chain(FORWARD_CHAIN);
+  klist_iterator* it;
+  filter* f;
+  unsigned int retval;
 
-	if (c.len == 0)
-		return NF_ACCEPT;
+  if (c.len == 0)
+    return NF_ACCEPT;
 
-	it = make_klist_iterator(c.filters);
-	while (klist_iterator_has_next(it)) {
-		f = (filter*) (klist_iterator_next(it))->data;
-		if (match_filter(f, skb)) {
-			retval = apply_filter(f, hooknum, skb, in, out, okfn);
-			if (retval != NF_ACCEPT) {
-				free_klist_iterator(it);
-				return retval;
-			}
-		}
-	}
+  it = make_klist_iterator(c.filters);
+  while (klist_iterator_has_next(it)) {
+    f = (filter*) (klist_iterator_next(it))->data;
+    if (match_filter(f, skb)) {
+      retval = apply_filter(f, hooknum, skb, in, out, okfn);
+      if (retval != NF_ACCEPT) {
 	free_klist_iterator(it);
+	return retval;
+      }
+    }
+  }
+  free_klist_iterator(it);
 
-	return NF_ACCEPT;
+  return NF_ACCEPT;
 }
 
 static unsigned int nf_ip_pre_routing_hook(unsigned int hooknum,
-		struct sk_buff *skb, const struct net_device *in,
-		const struct net_device *out, int(*okfn)(struct sk_buff*)) {
-	filter_chain c = get_chain(PRE_ROUTING_CHAIN);
-	klist_iterator* it;
-	filter* f;
-	unsigned int retval;
+					   struct sk_buff *skb, const struct net_device *in,
+					   const struct net_device *out, int(*okfn)(struct sk_buff*)) {
+  filter_chain c = get_chain(PRE_ROUTING_CHAIN);
+  klist_iterator* it;
+  filter* f;
+  unsigned int retval;
 
-	if (c.len == 0)
-		return NF_ACCEPT;
+  if (c.len == 0)
+    return NF_ACCEPT;
 
-	it = make_klist_iterator(c.filters);
-	while (klist_iterator_has_next(it)) {
-		f = (filter*) (klist_iterator_next(it))->data;
-		if (match_filter(f, skb)) {
-			retval = apply_filter(f, hooknum, skb, in, out, okfn);
-			if (retval != NF_ACCEPT) {
-				free_klist_iterator(it);
-				return retval;
-			}
-		}
-	}
+  it = make_klist_iterator(c.filters);
+  while (klist_iterator_has_next(it)) {
+    f = (filter*) (klist_iterator_next(it))->data;
+    if (match_filter(f, skb)) {
+      retval = apply_filter(f, hooknum, skb, in, out, okfn);
+      if (retval != NF_ACCEPT) {
 	free_klist_iterator(it);
+	return retval;
+      }
+    }
+  }
+  free_klist_iterator(it);
 
-	return NF_ACCEPT;
+  return NF_ACCEPT;
 }
 
 static unsigned int nf_ip_post_routing_hook(unsigned int hooknum,
-		struct sk_buff *skb, const struct net_device *in,
-		const struct net_device *out, int(*okfn)(struct sk_buff*)) {
-	filter_chain c = get_chain(POST_ROUTING_CHAIN);
-	klist_iterator* it;
-	filter* f;
-	unsigned int retval;
+					    struct sk_buff *skb, const struct net_device *in,
+					    const struct net_device *out, int(*okfn)(struct sk_buff*)) {
+  filter_chain c = get_chain(POST_ROUTING_CHAIN);
+  klist_iterator* it;
+  filter* f;
+  unsigned int retval;
 
-	if (c.len == 0)
-		return NF_ACCEPT;
+  if (c.len == 0)
+    return NF_ACCEPT;
 
-	it = make_klist_iterator(c.filters);
-	while (klist_iterator_has_next(it)) {
-		f = (filter*) (klist_iterator_next(it))->data;
-		if (match_filter(f, skb)) {
-			retval = apply_filter(f, hooknum, skb, in, out, okfn);
-			if (retval != NF_ACCEPT) {
-				free_klist_iterator(it);
-				return retval;
-			}
-		}
-	}
+  it = make_klist_iterator(c.filters);
+  while (klist_iterator_has_next(it)) {
+    f = (filter*) (klist_iterator_next(it))->data;
+    if (match_filter(f, skb)) {
+      retval = apply_filter(f, hooknum, skb, in, out, okfn);
+      if (retval != NF_ACCEPT) {
 	free_klist_iterator(it);
+	return retval;
+      }
+    }
+  }
+  free_klist_iterator(it);
 
-	return NF_ACCEPT;
+  return NF_ACCEPT;
 }
 
 void init_hooks(void) {
-	nf_ip_local_out.hook = nf_ip_local_out_hook;
-	nf_ip_local_out.pf = PF_INET;
-	nf_ip_local_out.hooknum = ip_local_out;
-	nf_ip_local_out.priority = NF_IP_PRI_FIRST;
-	nf_register_hook(&nf_ip_local_out);
+  nf_ip_local_out.hook = nf_ip_local_out_hook;
+  nf_ip_local_out.pf = PF_INET;
+  nf_ip_local_out.hooknum = ip_local_out;
+  nf_ip_local_out.priority = NF_IP_PRI_FIRST;
+  nf_register_hook(&nf_ip_local_out);
 
-	nf_ip_local_in.hook = nf_ip_local_in_hook;
-	nf_ip_local_in.pf = PF_INET;
-	nf_ip_local_in.hooknum = ip_local_in;
-	nf_ip_local_in.priority = NF_IP_PRI_FIRST;
-	nf_register_hook(&nf_ip_local_in);
+  nf_ip_local_in.hook = nf_ip_local_in_hook;
+  nf_ip_local_in.pf = PF_INET;
+  nf_ip_local_in.hooknum = ip_local_in;
+  nf_ip_local_in.priority = NF_IP_PRI_FIRST;
+  nf_register_hook(&nf_ip_local_in);
 
-	nf_ip_forward.hook = nf_ip_forward_hook;
-	nf_ip_forward.pf = PF_INET;
-	nf_ip_forward.hooknum = ip_forward;
-	nf_ip_forward.priority = NF_IP_PRI_FIRST;
-	nf_register_hook(&nf_ip_forward);
+  nf_ip_forward.hook = nf_ip_forward_hook;
+  nf_ip_forward.pf = PF_INET;
+  nf_ip_forward.hooknum = ip_forward;
+  nf_ip_forward.priority = NF_IP_PRI_FIRST;
+  nf_register_hook(&nf_ip_forward);
 
-	nf_ip_pre_routing.hook = nf_ip_pre_routing_hook;
-	nf_ip_pre_routing.pf = PF_INET;
-	nf_ip_pre_routing.hooknum = ip_pre_routing;
-	nf_ip_pre_routing.priority = NF_IP_PRI_FIRST;
-	nf_register_hook(&nf_ip_pre_routing);
+  nf_ip_pre_routing.hook = nf_ip_pre_routing_hook;
+  nf_ip_pre_routing.pf = PF_INET;
+  nf_ip_pre_routing.hooknum = ip_pre_routing;
+  nf_ip_pre_routing.priority = NF_IP_PRI_FIRST;
+  nf_register_hook(&nf_ip_pre_routing);
 
-	nf_ip_post_routing.hook = nf_ip_post_routing_hook;
-	nf_ip_post_routing.pf = PF_INET;
-	nf_ip_post_routing.hooknum = ip_post_routing;
-	nf_ip_post_routing.priority = NF_IP_PRI_FIRST;
-	nf_register_hook(&nf_ip_post_routing);
+  nf_ip_post_routing.hook = nf_ip_post_routing_hook;
+  nf_ip_post_routing.pf = PF_INET;
+  nf_ip_post_routing.hooknum = ip_post_routing;
+  nf_ip_post_routing.priority = NF_IP_PRI_FIRST;
+  nf_register_hook(&nf_ip_post_routing);
 }
 
 void free_hooks(void) {
-	nf_unregister_hook(&nf_ip_local_out);
-	nf_unregister_hook(&nf_ip_local_in);
-	nf_unregister_hook(&nf_ip_forward);
-	nf_unregister_hook(&nf_ip_pre_routing);
-	nf_unregister_hook(&nf_ip_post_routing);
+  nf_unregister_hook(&nf_ip_local_out);
+  nf_unregister_hook(&nf_ip_local_in);
+  nf_unregister_hook(&nf_ip_forward);
+  nf_unregister_hook(&nf_ip_pre_routing);
+  nf_unregister_hook(&nf_ip_post_routing);
 }
