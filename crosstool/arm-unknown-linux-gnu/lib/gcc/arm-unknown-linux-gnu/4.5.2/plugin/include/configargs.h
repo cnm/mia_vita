2a6d661c7b6651f840c54d69ca7ebb6a93b7b433
Joao Trindade <trindade.joao@gmail.com>
2011-12-27 15:23:09 +0000

    Added the crosstool

---
+++ crosstool/arm-unknown-linux-gnu/lib/gcc/arm-unknown-linux-gnu/4.5.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/root/7500/.build/src/gcc-4.5.2/configure --build=i686-build_pc-linux-gnu --host=i686-build_pc-linux-gnu --target=arm-unknown-linux-gnu --prefix=/root/x-tools/arm-unknown-linux-gnu --with-sysroot=/root/x-tools/arm-unknown-linux-gnu/arm-unknown-linux-gnu/sysroot --enable-languages=c,c++ --disable-multilib --with-pkgversion='crosstool-NG 1.12.1' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --with-gmp=/root/7500/.build/arm-unknown-linux-gnu/build/static --with-mpfr=/root/7500/.build/arm-unknown-linux-gnu/build/static --with-mpc=/root/7500/.build/arm-unknown-linux-gnu/build/static --with-ppl=/root/7500/.build/arm-unknown-linux-gnu/build/static --with-cloog=/root/7500/.build/arm-unknown-linux-gnu/build/static --with-libelf=/root/7500/.build/arm-unknown-linux-gnu/build/static --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm -L/root/7500/.build/arm-unknown-linux-gnu/build/static/lib -lpwl' --enable-threads=posix --enable-target-optspace --with-local-prefix=/root/x-tools/arm-unknown-linux-gnu/arm-unknown-linux-gnu/sysroot --disable-nls --enable-c99 --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
