d587af6acb889e886236daadd73376ce2997f845
Joao Trindade <trindade.joao@gmail.com>
2011-12-29 18:21:40 +0000

    More documentation

--- kernel_sender/README.rst
+++ kernel_sender/README.rst
@@ -10,3 +10,31 @@ This is the file structure:
 └── user                        - Userland program to receive the MIA Vita packets and to write them to two file (json and raw)
     ├── ...
     ...
+
+Objective
+=========
+
+    Kernel sender is a module application which reads data from the buffer created at interruption/proc_entry.c and sends it to the other nodes.
+
+Drawing
+=======
+
+Logic behind the synchronization::
+
+
+ +---------+                                             +---------+                         +---------+
+ |   App   |  (1)        (Kernel Module)                 |   App   |                     (1) |   App   |
+ |---------|   |                                         |---------|                      ↑  |---------|
+ |  Trans  |   |         (UDP)                           |  Trans  |                      |  |  Trans  |
+ |---------|   |                                         |---------|                      |  |---------|
+ |  Inter  |   |         (IP)                            |  Inter  |                      |  |  Inter  |
+ |---------|   |                                         |---------|                      |  |---------|
+ |         |   ↓                                       ↔ |         | ↔                    |  |         |
+ |  Link   |  (1->2)     (MAC - Kernel Module)    (2->1) |  Link   | (1->2)           (2->1) |  Link   |
+ |         |   --------------------------------------->  |         | --------------------->  |         |
+ +---------+                                             +---------+                         +---------+
+
+ 1    )  Happens at the interruption at proc_entry.c  (function: read_four_channels in interruption/fpga.c)
+ 1-2  )  Happens in the rt2501 module                 (function: synch_out_data_packet in rt2501/sources/Module/sync_proto.c)
+ 2-1  )  Happens at the rt2501 module                 (function: synch_in_data_packet  in rt2501/sources/Module/sync_proto.c)
+
--- kernel_sender/miavita_packet.h
+++ kernel_sender/miavita_packet.h
@@ -21,13 +21,31 @@ typedef struct __attribute__ ((__packed__)){ // specifies that the minimum requi
   int64_t gps_us;                            // where to store the GPS time
 #endif
 
-  int64_t timestamp;                         // Signed Transmission time since packet was created
+  int64_t timestamp;                         // Signed Transmission time the packet was created (1) or the time since packet was created (2)
   int64_t air;                               // Estimated time the packet was "on the air"
   uint32_t seq;                              // Sequence number
   uint8_t fails;                             // Fails since last packet was received
   uint8_t retries;                           // Retries ...
   uint8_t samples[12];                       // Samples for the four channels
   uint8_t id;                                // ID of the originator node
-}packet_t;
+} packet_t;
 
+/*
+ *  +---------+                                             +---------+                         +---------+
+ *  |   App   |  (1)        (Kernel Module)                 |   App   |                     (1) |   App   |
+ *  |---------|   |                                         |---------|                      ↑  |---------|
+ *  |  Trans  |   |         (UDP)                           |  Trans  |                      |  |  Trans  |
+ *  |---------|   |                                         |---------|                      |  |---------|
+ *  |  Inter  |   |         (IP)                            |  Inter  |                      |  |  Inter  |
+ *  |---------|   |                                         |---------|                      |  |---------|
+ *  |         |   ↓                                       ↔ |         | ↔                    |  |         |
+ *  |  Link   |  (1->2)     (MAC - Kernel Module)    (2->1) |  Link   | (1->2)           (2->1) |  Link   |
+ *  |         |   --------------------------------------->  |         | --------------------->  |         |
+ *  +---------+                                             +---------+                         +---------+
+ *
+ *
+ * 1    )  Happens at the interruption at proc_entry.c  (function: read_four_channels in interruption/fpga.c)
+ * 1-2  )  Happens in the rt2501 module                 (function: synch_out_data_packet in rt2501/sources/Module/sync_proto.c)
+ * 2-1  )  Happens at the rt2501 module                 (function: synch_in_data_packet  in rt2501/sources/Module/sync_proto.c)
+ */
 #endif
