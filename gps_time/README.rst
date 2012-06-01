88b7593b1a982dc94ca2ef329627a3d5e50d117d
Joao Trindade <trindade.joao@gmail.com>
2012-06-01 20:35:51 +0100

    Added instructions

--- gps_time/README.rst
+++ gps_time/README.rst
@@ -1,4 +1,11 @@
 GPS Utilities
 -------------
 
-TODO
+
+
+To run
+______
+
+./uart_gps_test
+xuartctl -p 0 -o 8o1 -s 9600 -d
+
--- gps_time/uart_gps_test.c
+++ gps_time/uart_gps_test.c
@@ -9,37 +9,48 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 #include "gps_time.h"
 #include "gps_uartctl.h"
+#include "gps_uartctl.h"
 
 /* Subtract the `struct timeval' values X and Y,
- storing the result in RESULT.
- Return 1 if the difference is negative, otherwise 0.  */
+   storing the result in RESULT.
+   Return 1 if the difference is negative, otherwise 0.  */
 
 
 int main(int argc, char **argv) {
-	int x;
-	struct tm *broken_time;
-	struct timeval t;
+    unsigned int i;
+    for(i = 1; i < argc;){
+        if(!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")){
+            printf("This is the help function");
+            return 0;
+        }
+    }
 
-	uart_init(1, stderr);
 
-	while (!is_gps_ready()) {
-		sleep(1);
-	}
+    int x;
+    struct tm *broken_time;
+    struct timeval t;
 
-	printf("GPS is ready\n");
+    uart_init(1, stderr);
 
-	for (x = 0; x < 100; x++) {
-		getGPStimeUTC(&t);
-		broken_time = localtime(&(t.tv_sec));
+    while (!is_gps_ready()) {
+        sleep(1);
+    }
 
-		//printf("%ld sec %ld usec\n", t.tv_sec, t.tv_usec);
-		printf("Current date: %02d/%02d/%02d %02d:%02d:%02d.%06d\n",
-				broken_time->tm_mday, broken_time->tm_mon + 1,
-				broken_time->tm_year + 1900, broken_time->tm_hour,
-				broken_time->tm_min, broken_time->tm_sec, (int) t.tv_usec);
-	}
-	return 0;
+    printf("GPS is ready\n");
+
+    for (x = 0; x < 100; x++) {
+        getGPStimeUTC(&t);
+        broken_time = localtime(&(t.tv_sec));
+
+        //printf("%ld sec %ld usec\n", t.tv_sec, t.tv_usec);
+        printf("Current date: %02d/%02d/%02d %02d:%02d:%02d.%06d\n",
+               broken_time->tm_mday, broken_time->tm_mon + 1,
+               broken_time->tm_year + 1900, broken_time->tm_hour,
+               broken_time->tm_min, broken_time->tm_sec, (int) t.tv_usec);
+    }
+    return 0;
 }
