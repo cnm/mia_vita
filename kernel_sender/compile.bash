35352af8940d246befe8f13f834a2fcf2945b0f4
Joao Trindade <trindade.joao@gmail.com>
2012-06-21 11:43:01 +0100

    Continue to making relative paths

--- kernel_sender/compile.bash
+++ kernel_sender/compile.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-make KERNEL_PATH=/lib/modules/2.6.24.4/build clean
+make KERNEL_PATH=../ts7500_kernel/ clean
 cd ../interruption; make clean; make; cp Module.symvers ../kernel_sender; cd -
-make KERNEL_PATH=/lib/modules/2.6.24.4/build
+make KERNEL_PATH=../ts7500_kernel/
