cf4a1f57fff4a281278c982d648e7e7fe2efc069
Joao Trindade <trindade.joao@gmail.com>
2012-06-22 14:39:09 +0100

    Idented list.c

--- kernel_sender/user/list.c
+++ kernel_sender/user/list.c
@@ -15,39 +15,42 @@ char* output_json_file = "miavita.json";
 int bin_fd = -1, json_fd = -1;
 
 list* mklist(uint32_t capacity, char* new_filename){
-  list* l = alloc(list, 1);
-  l->buff = alloc(packet_t, capacity);
-  l->rotate_at = capacity;
-  l->new_filename = new_filename;
+    list* l = alloc(list, 1);
+    l->buff = alloc(packet_t, capacity);
+    l->rotate_at = capacity;
+    l->new_filename = new_filename;
 }
 
 void rmlist(list* l){
-  if(l){
-    free(l->buff);
-    free(l);
-  }
+    if(l)
+      {
+        free(l->buff);
+        free(l);
+      }
 }
 
 static void write_bin(packet_t pkt){
-  int32_t to_write = sizeof(pkt), status, written = 0;
-  while(written < to_write){
-    status = write(bin_fd, ((char*) &pkt) + written, to_write - written);
-    if(status == -1){
-      perror("Unable to write binary data.\n");
-      return;
-    }
-    written += status;
-  }
+    int32_t to_write = sizeof(pkt), status, written = 0;
+    while(written < to_write)
+      {
+        status = write(bin_fd, ((char*) &pkt) + written, to_write - written);
+        if(status == -1)
+          {
+            perror("Unable to write binary data.\n");
+            return;
+          }
+        written += status;
+      }
 }
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 #define sample_to_le(S)				\
   do{						\
-    uint8_t* ___s = (uint8_t*) (S);		\
-    uint8_t ___t[3] = {0};			\
-    memcpy(___t, ___s, 3);			\
-    ___s[0] = ___t[2];				\
-    ___s[2] = ___t[0];				\
+      uint8_t* ___s = (uint8_t*) (S);		\
+      uint8_t ___t[3] = {0};			\
+      memcpy(___t, ___s, 3);			\
+      ___s[0] = ___t[2];				\
+      ___s[2] = ___t[0];				\
   }while(0)
 #else
 #define sample_to_le(S)
@@ -55,19 +58,20 @@ static void write_bin(packet_t pkt){
 
 static packet_t ntohpkt(packet_t pkt){
 #ifdef __GPS__
-  pkt.gps_us = be64toh( pkt.gps_us );
+    pkt.gps_us = be64toh( pkt.gps_us );
 #endif
-  pkt.timestamp = be64toh( pkt.timestamp );
-  pkt.air = be64toh( pkt.air );
-  pkt.seq = be32toh( pkt.seq );
-  sample_to_le(pkt.samples);
-  sample_to_le(pkt.samples + 3);
-  sample_to_le(pkt.samples + 6);
-  sample_to_le(pkt.samples + 9);
-  return pkt;
+    pkt.timestamp = be64toh( pkt.timestamp );
+    pkt.air = be64toh( pkt.air );
+    pkt.seq = be32toh( pkt.seq );
+    sample_to_le(pkt.samples);
+    sample_to_le(pkt.samples + 3);
+    sample_to_le(pkt.samples + 6);
+    sample_to_le(pkt.samples + 9);
+    return pkt;
 }
 
-static void write_json(packet_t pkt){
+static void write_json(packet_t pkt)
+{
   static uint8_t first = 1;
   char buff[2048] = {0};
   uint32_t to_write, written = 0, status;
@@ -75,12 +79,14 @@ static void write_json(packet_t pkt){
 
   pkt = ntohpkt(pkt);
 
-  if(!first)
-    write(json_fd, ",\n", 2);
-  else{
-    write(json_fd, "\n", 1);
-    first = 0;
+  if(!first){
+      write(json_fd, ",\n", 2);
   }
+  else
+    {
+      write(json_fd, "\n", 1);
+      first = 0;
+    }
 
   memcpy(&sample1, pkt.samples, 3);
   memcpy(&sample2, pkt.samples + 3, 3);
@@ -88,52 +94,60 @@ static void write_json(packet_t pkt){
   memcpy(&sample4, pkt.samples + 9, 3);
 
 #ifdef __GPS__
-  if(test){
-    struct timeval t;
-    gettimeofday(&t, NULL);
+  if(test)
+    {
+      struct timeval t;
+      gettimeofday(&t, NULL);
 
-    pkt.timestamp = (((int64_t) t.tv_sec) * 1000000 + t.tv_usec) - pkt.timestamp;
-    pkt.gps_us = get_millis_offset() - pkt.gps_us;
-  }
+      pkt.timestamp = (((int64_t) t.tv_sec) * 1000000 + t.tv_usec) - pkt.timestamp;
+      pkt.gps_us = get_millis_offset() - pkt.gps_us;
+    }
   sprintf(buff, "\"%u:%u\" : {\"gps_us\" : %lld, \"timestamp\" : %lld, \"air_time\" : %lld, \"sequence\" : %u, \"fails\" : %u, \"retries\" : %u, \"sample_1\" : %u, \"sample_2\" : %u, \"sample_3\" : %u, \"sample_4\" : %u \"node_id\" : %u }", pkt.id, pkt.seq, pkt.gps_us, pkt.timestamp, pkt.air, pkt.seq, pkt.fails, pkt.retries, sample1, sample2, sample3, sample4, pkt.id);
 #else
   sprintf(buff, "\"%u:%u\" : {\"timestamp\" : %lld, \"air_time\" : %lld, \"sequence\" : %u, \"fails\" : %u, \"retries\" : %u, \"sample_1\" : %u, \"sample_2\" : %u, \"sample_3\" : %u, \"sample_4\" : %u \"node_id\" : %u }", pkt.id, pkt.seq, pkt.timestamp, pkt.air, pkt.seq, pkt.fails, pkt.retries, sample1, sample2, sample3, sample4, pkt.id);
 #endif
   to_write = strlen(buff);
-  while(written < to_write){
-    status = write(json_fd, buff + written, to_write - written);
-    if(status == -1){
-      perror("Unable to write to json file");
-      return;
+  while(written < to_write)
+    {
+      status = write(json_fd, buff + written, to_write - written);
+      if(status == -1)
+        {
+          perror("Unable to write to json file");
+          return;
+        }
+      written += status;
     }
-    written += status;
-  }
 }
 
-static uint8_t open_output_files(){
+static uint8_t open_output_files()
+{
   bin_fd = open(output_binary_file, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-  if(bin_fd == -1){
-    perror("Unable to open binary output file");
-    return 0;
-  }
+  if(bin_fd == -1)
+    {
+      perror("Unable to open binary output file");
+      return 0;
+    }
 
   json_fd = open(output_json_file, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-  if(json_fd == -1){
-    close(bin_fd);
-    perror("Unable to open json output file");
-    return 0;
-  }
+  if(json_fd == -1)
+    {
+      close(bin_fd);
+      perror("Unable to open json output file");
+      return 0;
+    }
   write(json_fd, "{", 1);
   return 1;
 }
 
-static void close_output_files(){
+static void close_output_files()
+{
   close(bin_fd);
   write(json_fd, "\n}", 2);
   close(json_fd);
 }
 
-static void dump(list* l){
+static void dump(list* l)
+{
   char buff[100] = {0};
   uint32_t i;
 
@@ -143,37 +157,40 @@ static void dump(list* l){
   memset(buff, 0, sizeof(buff));
   sprintf(buff, "%s.json", l->new_filename);
   rename(output_json_file, buff);
-  
-  if(open_output_files()){
-    for(i = 0; i < l->lst_size; i++){
-      write_bin(l->buff[i]);
-      write_json(l->buff[i]);
+
+  if(open_output_files())
+    {
+      for(i = 0; i < l->lst_size; i++)
+        {
+          write_bin(l->buff[i]);
+          write_json(l->buff[i]);
+        }
+      close_output_files();
     }
-    close_output_files();
-  }
 }
 
 #warning "This implementation of __packet_comparator has bugs. It is not receiving the correct memory address of a and b."
 //Because timestamp is int64_t and this should return int we cannot just do a.timestamp - b.timestamp.
 int  __packet_comparator(const void* a, const void* b){
-  int64_t at = ((packet_t*) a)->timestamp;
-  int64_t bt = ((packet_t*) b)->timestamp;
+    int64_t at = ((packet_t*) a)->timestamp;
+    int64_t bt = ((packet_t*) b)->timestamp;
 
-  printf("A seq: %u\n", ((packet_t*) a)->seq);
+    printf("A seq: %u\n", ((packet_t*) a)->seq);
 
-  if(at < bt){
-    printf("%lld < %lld\n", at, bt);
-    return -1;
-  }
-  if(at > bt){
-    printf("%lld > %lld\n", at, bt);
-    return 1;
-  }
-  printf("%lld == %lld\n", at, bt);
-  return 0;
+    if(at < bt){
+        printf("%lld < %lld\n", at, bt);
+        return -1;
+    }
+    if(at > bt){
+        printf("%lld > %lld\n", at, bt);
+        return 1;
+    }
+    printf("%lld == %lld\n", at, bt);
+    return 0;
 }
 
-static void rotate(list* l){
+static void rotate(list* l)
+{
   /*
    * Every packet in the buffer is sorted, but only half of the packets are written to file.
    * This is to avoid (not completed) that packets which arrive to late are also sorted correctly.
@@ -187,11 +204,13 @@ static void rotate(list* l){
   memmove(l->buff, l->buff + l->lst_size + 1, l->rotate_at - l->lst_size);
 }
 
-void insert(list* l, packet_t* p){
-
+void insert(list* l, packet_t* p)
+{
   memcpy(l->buff + l->lst_size, p, sizeof(*p));
 
   l->lst_size++;
   if(l->rotate_at == l->lst_size)
-    rotate(l);
+    {
+      rotate(l);
+    }
 }
