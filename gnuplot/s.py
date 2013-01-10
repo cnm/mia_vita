a38a69cece496cab8b8843bb224376bdc64e01bb
Joao Trindade <trindade.joao@gmail.com>
2013-01-10 11:38:48 +0000

    Plot with a lot of cleanup

--- gnuplot/s.py
+++ gnuplot/s.py
@@ -16,7 +16,7 @@ def get_values():
             c3 = data[k]["3"]
             c4 = data[k]["4"]
 
-            values[node_id].append((c1, c2, c3, c4, seq))
+            values[node_id].append([c1, c2, c3, c4, seq])
 
     with open('miavita.json.3') as data_file:
         data = json.load(data_file)
@@ -28,7 +28,7 @@ def get_values():
             c3 = data[k]["3"]
             c4 = data[k]["4"]
 
-            values[node_id].append((c1, c2, c3, c4, seq))
+            values[node_id].append([c1, c2, c3, c4, seq])
     return values
 
 def draw_values(val):
@@ -48,7 +48,7 @@ def draw_values(val):
         plt.plot(channel1, label="x")
         plt.plot(channel2, label="y")
         plt.plot(channel3, label="z")
-        plt.ylim(-2500000, 2500000)
+        plt.ylim(-1200000, 1200000)
 
         if 1 == int(node):
             a.set_title("Sink Node: " + str(node))
@@ -81,58 +81,92 @@ def get_file():
     call(["wget", url2])
     call(["wget", url3])
 
-def join_values(previous, val):
-    for k in previous.keys():
-        previous[k] += (val[k])
-
 def limit_size(previous, maxi):
     for k in previous.keys():
         n = len(previous[k])
-        if(n > maxi):
+        if(n >= maxi):
             previous[k] = previous[k][n - maxi:] #Discard older elements
             print "Previous: " + str(n) + " - " + str(maxi)
-        print "Len result" + str(len(previous[k]))
         print
 
-def clean_values(new, previous, last_seq):
+def is_outlier(index, l):
+    if(not l):
+        return False
+
+    lenght = len(l)
+    n = 10
+
+    to_compare = range(index + 1, min(index + 1 + n, len(l)))
+
+    if(len(to_compare) < n):
+        to_compare += range(max(0, index - (n - len(to_compare))), index)
+
+    total = 0
+    for i in to_compare:
+        if (i + 1 == len(l)): continue
+#        print str(l[i][3]) + " " + str(l[i+1][3])
+        total = abs(l[i][3] - l[i+1][3])
+
+    beta = float(10000)
+
+    if(index > 0):
+        if total * beta < abs(l[index][3] - l[index -1][3]):
+            print total
+            return True
+        else:
+            return False
+    else:
+        if total * beta < abs(l[index][3] - l[index + 1][3]):
+            print total
+            return True
+        else:
+            return False
+
+def clean_values(new, previous, last_seq, means):
+
     for k in new.keys():
+        # Sort new[k] by sequence number
+        new[k].sort(key=lambda x: x[4])
+
+        i = 0
 
         for n in new[k]:
             seq_n_new = n[4]
+
+            if is_outlier(i, new[k]):
+                print "OUTLIER"
+            i += 1
 
             # Ignore repeated values
             if(seq_n_new <= last_seq[k]):
                 continue
 
-            # Insert missing values
             for x in xrange(last_seq[k] + 1, seq_n_new):
-                previous[k].append((0,0,0,0,x))
+                previous[k].append([0,0,0,0,x])
+
+            alpha = float(0.8)
+
+            n[3] = means[k][3] = means[k][3] * alpha + n[3] * (1 - alpha)
 
             last_seq[k] = seq_n_new
             previous[k].append(n)
 
-            # Sort new[k]
-            new[k].sort(key=lambda x: x[4])
-
-    print "After " + str(last_seq)
-
-def run(previous, last_seq):
+def run(previous, last_seq, means):
     while(True):
-   #         get_file()
+            get_file()
             val = get_values()
- #           join_values(previous, val)
-            clean_values(val, previous, last_seq)
+            clean_values(val, previous, last_seq, means)
             limit_size(previous, 1500)
             draw_values(previous)
             sleep(0.5)
 
 previous = {1:[], 2:[], 3:[]}
 last_seq = {1:0, 2:0, 3:0}
+means = {1:[0,0,0,0], 2:[0,0,0,0], 3:[0,0,0,0]}
 while True:
-   try:
-        print "Weird: " + str(len(previous[1]))
-        run(previous, last_seq)
-   except Exception as e:
+#   try:
+        run(previous, last_seq, means)
+#   except Exception as e:
         print "###################################"
         print "Except" + str(e)
         print "###################################"
